3e904999b45cda17c6f6f0e04d2b84d0072885c7
DanielD26 <103547416@student.swin.edu.au>
2021-03-18 14:42:23 +1100

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Selections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter age: ");
+            string age = Console.ReadLine();
+
+            int ageNum = int.Parse(age);
+
+            if (ageNum > 17) {
+
+                Console.Write("Please enter your gender: ");
+                string gender = Console.ReadLine();
+
+                if (gender == "M") {
+                    Console.WriteLine("M is an adult");
+                } else if (gender == "F"){
+                    Console.WriteLine("F is an adult");
+                } else {
+                    Console.WriteLine("Please enter M or F");
+                }
+            } else {
+                Console.WriteLine("Legally not adult age");
+            }
+        }
+    }
+}
